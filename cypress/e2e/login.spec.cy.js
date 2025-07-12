836200887203538ab570f93fcc16a7736039132a
brunomoscoso <158348569+brunomoscoso@users.noreply.github.com>
2025-07-12 10:13:04 +0100

    Adding user data json

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,23 +1,25 @@
+import userData from '../fixtures/users/userData.json';
+
 describe('Orange HRM Tests', () => {
 
   const selectorsLsit = {
     usernameField: "[name='username']",
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
-    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
+    dashboardGrid: '.orangehrm-dashboard-grid',
     wrongCredentialAlert: "[role='alert']"
   };
+
 
   it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-    cy.get(selectorsLsit.usernameField).type('Admin');
-    cy.get(selectorsLsit.passwordField).type('admin123');
+    cy.get(selectorsLsit.usernameField).type(userData.userSuccess.username);
+    cy.get(selectorsLsit.passwordField).type(userData.userSuccess.password);
     cy.get(selectorsLsit.loginButton).click();
 
     // Espera o Dashboard carregar (esperando um elemento confiável do dashboard)
-    cy.get(selectorsLsit.sectionTitleTopBar, { timeout: 10000 }).should('contain', 'Dashboard');
-
+    cy.get(selectorsLsit.dashboardGrid, { timeout: 10000 })
     // Verifica se o pathname está correto
     cy.location('pathname').should('eq', '/web/index.php/dashboard/index');
     cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard');
@@ -26,8 +28,8 @@ describe('Orange HRM Tests', () => {
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-    cy.get(selectorsLsit.usernameField).type('Test');
-    cy.get(selectorsLsit.passwordField).type('Test');
+    cy.get(selectorsLsit.usernameField).type(userData.userFail.username);
+    cy.get(selectorsLsit.passwordField).type(userData.userFail.password);
     cy.get(selectorsLsit.loginButton).click();
     cy.get(selectorsLsit.wrongCredentialAlert);
   });
